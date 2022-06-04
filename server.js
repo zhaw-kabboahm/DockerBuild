53cfa99b269a191ebaff061d405d921a4d6eea8b
zhaw-kabboahm <33002091+zhaw-kabboahm@users.noreply.github.com>
2022-06-04 10:46:55 +0200

    ADDED Heroku ports

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict';
 const express = require('express');
 // Constants
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello DevOps Course, Ahmad hat jetzt übernommen :D - Sogar min Jenkins ;)');
+res.send('Hello DevOps Course :-)))');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
